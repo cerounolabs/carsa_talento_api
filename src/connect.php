99b6bc7251c0df9e39bd10c1b226b4bf738df6fa
Christian Zelaya <christian@cerouno.com.py>
2019-09-20 09:45:00 -0400

    Update v1.2

--- src/connect.php
+++ src/connect.php
@@ -25,8 +25,8 @@
         $serverName = "192.168.16.17";
         $serverPort = "";
         $serverDb   = "carsa_talento";
-        $serverUser = "root";
-        $serverPass = "prabhupada1A+";
+        $serverUser = "talento_admin";
+        $serverPass = "dlFGaCp2v48iPjHK";
         
         try {
             $conn = new PDO("mysql:host=$servername;dbname=$serverDb;charset=utf8", $serverUser, $serverPass,
