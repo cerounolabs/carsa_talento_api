c5e48ecb80e3983c5a96d4c5b9e26b303dce2438
Christian Zelaya <christian@cerouno.com.py>
2019-09-20 10:54:23 -0400

    v1.5 update directorio composer

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@
 	use \Psr\Http\Message\ServerRequestInterface as Request;
 	use \Psr\Http\Message\ResponseInterface as Response;
 
-	require __DIR__.'/../../slim/vendor/autoload.php';
+	require __DIR__.'/../vendor/autoload.php';
 	$settings = require __DIR__.'/../src/settings.php';
 
 	$app = new \Slim\App($settings);
