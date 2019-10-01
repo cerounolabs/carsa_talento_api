b00002185d9a956314166677a6d28f4e52491454
Christian Zelaya <christian@cerouno.com.py>
2019-10-01 08:23:54 -0400

    Bugs Conexion MSSQL

--- src/connect.php
+++ src/connect.php
@@ -7,7 +7,7 @@
         $serverPass = "carsa_2019";
 
         try {
-            $conn = new PDO("sqlsrv:Server=$serverName,$serverPort;Database=$serverDb", $serverUser, $serverPass,
+            $conn = new PDO("sqlsrv:Server=$serverName,$serverPort;Database=$serverDb;ConnectionPooling=0", $serverUser, $serverPass,
                 array(
                     PDO::ATTR_PERSISTENT => true,
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
