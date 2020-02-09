a8dcdfb66ffa93c2660fa2af54ad7b6863272ff5
Christian Zelaya - PC-01 <christian@cerouno.com.py>
2020-02-08 23:57:19 -0300

    Fix MIGRACION PGSQL

---
+++ process/migracion.php
@@ -0,0 +1,66 @@
+<?php
+    require __DIR__.'/../src/connect.php';
+
+    function getEstadoCivil(){
+        $DOMFICEST  = 1;
+        $DOMFICNOM  = '';
+        $DOMFICEQU  = '';
+        $DOMFICVAL  = 'ESTADOCIVIL';
+        $DOMFICOBS  = '';
+        $DOMFICAUS  = 'MIGRACION';
+        $DOMFICAFH  = date('Y-m-d H:i:s');
+        $DOMFICAIP  = $_SERVER['REMOTE_ADDR'];
+
+        $sql00      = "SELECT a.ECCod AS estado_civil_codigo, a.ECDsc AS estado_civil_nombre FROM ESTCIV a ORDER BY a.ECDsc";
+        $sql01      = "SELECT * FROM DOMFIC WHERE DOMFICEQU = ? AND DOMFICVAL = ?";
+        $sql02      = "INSERT INTO DOMFIC (DOMFICEST, DOMFICNOM, DOMFICEQU, DOMFICVAL, DOMFICOBS, DOMFICAUS, DOMFICAFH, DOMFICAIP) VALUES (?, ?, ?, ?, ?, ?, NOW(), ?)";
+
+        try {
+            $connMSSQL  = getConnectionMSSQLv2();
+            $connPGSQL  = getConnectionPGSQLv1();
+
+            $stmtMSSQL  = $connMSSQL->prepare($sql00);
+            $stmtMSSQL->execute();
+
+            $stmtPGSQL1 = $connPGSQL->prepare($sql01);
+            $stmtPGSQL2 = $connPGSQL->prepare($sql02);
+
+            while ($rowMSSQL = $stmtMSSQL->fetch()) {
+                $DOMFICEQU = $rowMSSQL['estado_civil_codigo'];
+                $DOMFICNOM = trim($rowMSSQL['estado_civil_nombre']);
+
+                $stmtMPGSQL1->execute([$DOMFICEQU, $DOMFICVAL]);
+
+                $rowPGSQL1 = $stmtPGSQL1->fetch(PDO::FETCH_ASSOC);
+                    
+                if (!$rowPGSQL1){
+                    $stmtPGSQL2->execute([$DOMFICEST, $DOMFICNOM, $DOMFICEQU, $DOMFICVAL, $DOMFICOBS, $DOMFICAUS, $DOMFICAIP]);
+                }
+            }
+
+            $stmtMSSQL->closeCursor();
+            $stmtPGSQL1->closeCursor();
+            $stmtPGSQL2->closeCursor();
+
+            $stmtMSSQL  = null;
+            $stmtPGSQL1 = null;
+            $stmtPGSQL2 = null;
+        } catch (PDOException $e) {
+            echo 'Error setDepartamento(): '.$e;
+        }
+
+        $connMSSQL  = null;
+        $connPGSQL  = null;
+    }
+
+    echo "\n";
+    echo "++++++++++++++++++++++++++PROCESO DE MIGRACIÓN++++++++++++++++++++++++++";
+    echo "\n";
+    echo "++++++++++++++++ SISTEMA CORPORATIVO => SISTEMA TALENTO ++++++++++++++++";
+    echo "\n";
+    echo "INICIO getEstadoCivil() => ".date('Y-m-d H:i:s');
+    getEstadoCivil();
+    echo "\n";
+    echo "FIN getEstadoCivil() => ".date('Y-m-d H:i:s');
+    echo "\n";
+?>
--- src/connect.php
+++ src/connect.php
@@ -46,3 +46,97 @@
 
         return $conn;
     }
+
+    function getConnectionMSSQLv1(){
+        $serverName = "SRVDESA01";
+        $serverName = "192.168.16.9";
+        $serverPort = "1433";
+        $serverDb   = "PRODUCCION_AYER";
+        $serverUser = "czelaya";
+        $serverPass = "carsa_2019";
+        
+        try {
+            $conn = new PDO("sqlsrv:Server=$serverName,$serverPort;Database=$serverDb;ConnectionPooling=0", $serverUser, $serverPass,
+                array(
+                    PDO::ATTR_PERSISTENT => false,
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                )
+            );
+        } catch (PDOException $e) {
+            header("Content-Type: application/json; charset=utf-8");
+            echo json_encode(array('code' => 204, 'status' => 'failure', 'message' => 'Error Connecting to MSSQL: '.$e), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+            die();
+        }
+
+        return $conn;
+    }
+
+    function getConnectionMSSQLv2(){
+        $serverName = "SRVDESA01";
+        $serverName = "192.168.16.9";
+        $serverPort = "1433";
+        $serverDb   = "PRODUCCION_AYER";
+        $serverUser = "czelaya";
+        $serverPass = "carsa_2019";
+        
+        try {
+            $conn = new PDO("sqlsrv:Server=$serverName,$serverPort;Database=$serverDb;ConnectionPooling=0", $serverUser, $serverPass,
+                array(
+                    PDO::ATTR_PERSISTENT => false,
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                )
+            );
+        } catch (PDOException $e) {
+            header("Content-Type: application/json; charset=utf-8");
+            echo json_encode(array('code' => 204, 'status' => 'failure', 'message' => 'Error Connecting to MSSQL: '.$e), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+            die();
+        }
+
+        return $conn;
+    }
+
+    function getConnectionPGSQLv1(){
+        $serverName = "localhost";
+        $serverPort = "5432";
+        $serverDb   = "thholox_v10";
+        $serverUser = "user_thholox";
+        $serverPass = "ns3r_thh0l0x";
+        
+        try {
+            $conn = new PDO("pgsql:host=$serverName;port=$serverPort;dbname=$serverDb", $serverUser, $serverPass,
+                array(
+                    PDO::ATTR_PERSISTENT => true,
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                )
+            );
+        } catch (PDOException $e) {
+            header("Content-Type: application/json; charset=utf-8");
+            echo json_encode(array('code' => 204, 'status' => 'failure', 'message' => 'Error Connecting to PGSQLv1: '.$e), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+            die();
+        }
+
+        return $conn;
+    }
+
+    function getConnectionPGSQLv2(){
+        $serverName = "localhost";
+        $serverPort = "5432";
+        $serverDb   = "thholox_v20";
+        $serverUser = "user_thholox";
+        $serverPass = "ns3r_thh0l0x";
+        
+        try {
+            $conn = new PDO("pgsql:host=$serverName;port=$serverPort;dbname=$serverDb", $serverUser, $serverPass,
+                array(
+                    PDO::ATTR_PERSISTENT => true,
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                )
+            );
+        } catch (PDOException $e) {
+            header("Content-Type: application/json; charset=utf-8");
+            echo json_encode(array('code' => 204, 'status' => 'failure', 'message' => 'Error Connecting to PGSQLv2: '.$e), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+            die();
+        }
+
+        return $conn;
+    }
