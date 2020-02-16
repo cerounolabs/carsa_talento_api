1125ef4ed2280421bb09311c20be9055d28236d1
Christian Zelaya - PC-01 <christian@cerouno.com.py>
2020-02-16 08:13:47 -0300

    Fix CONEXION SISTEMA

--- app/put.php
+++ app/put.php
@@ -169,7 +169,7 @@
         return $json;
     });
 
-    $app->post('/v1/400/acceso/{codigo}', function($request) {
+    $app->put('/v1/400/acceso/{codigo}', function($request) {
         require __DIR__.'/../src/connect.php';
 
         $val00      = $request->getAttribute('codigo');
