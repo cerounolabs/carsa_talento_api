2057e397f967baece690fd53e3471a8cd5e3de08
Christian Zelaya - PC-01 <christian@cerouno.com.py>
2020-03-25 22:07:11 -0400

    Feature Auth

--- public/index.php
+++ public/index.php
@@ -28,83 +28,9 @@
 	//ROUTES
 	require __DIR__.'/../src/routes.php';
 
-		$app->get('/v1/000/dominio', function($request) {
-			require __DIR__.'/../src/connect.php';
-			
-			$sql00  = "SELECT
-			a.DOMFICCOD                     AS      tipo_codigo,
-			a.DOMFICORD                     AS      tipo_orden,
-			a.DOMFICNOM                     AS      tipo_nombre,
-			a.DOMFICEQU                     AS      tipo_equivalente,
-			a.DOMFICVAL                     AS      tipo_dominio,
-			a.DOMFICOBS                     AS      tipo_observacion,
-			a.DOMFICAUS                     AS      auditoria_usuario,
-			a.DOMFICAFH                     AS      auditoria_fecha_hora,
-			a.DOMFICAIP                     AS      auditoria_ip,
-	
-			b.DOMFICCOD                     AS      tipo_estado_codigo,
-			b.DOMFICNOM                     AS      tipo_estado_nombre
-	
-			FROM sistema.DOMFIC a
-			INNER JOIN sistema.DOMFIC b ON a.DOMFICEST = b.DOMFICCOD
-	
-			ORDER BY a.DOMFICORD, a.DOMFICNOM";
-	
-			try {
-				$connPGSQL  = getConnectionPGSQLv1();
-				$stmtPGSQL  = $connPGSQL->prepare($sql00);
-				$stmtPGSQL->execute([]); 
-	
-				while ($rowPGSQL = $stmtPGSQL->fetch()) {
-					$detalle    = array(
-						'tipo_codigo'           => $rowPGSQL['tipo_codigo'],
-						'tipo_estado_codigo'    => $rowPGSQL['tipo_estado_codigo'],
-						'tipo_estado_nombre'    => strtoupper(strtolower(trim($rowPGSQL['tipo_estado_nombre']))),
-						'tipo_orden'            => $rowPGSQL['tipo_orden'],
-						'tipo_nombre'           => strtoupper(strtolower(trim($rowPGSQL['tipo_nombre']))),
-						'tipo_equivalente'      => strtoupper(strtolower(trim($rowPGSQL['tipo_equivalente']))),
-						'tipo_dominio'          => strtoupper(strtolower(trim($rowPGSQL['tipo_dominio']))),
-						'tipo_observacion'      => strtoupper(strtolower(trim($rowPGSQL['tipo_observacion']))),
-						'auditoria_usuario'     => strtoupper(strtolower(trim($rowPGSQL['auditoria_usuario']))),
-						'auditoria_fecha_hora'  => $rowPGSQL['auditoria_fecha_hora'],
-						'auditoria_ip'          => strtoupper(strtolower(trim($rowPGSQL['auditoria_ip'])))
-					);
-	
-					$result[]   = $detalle;
-				}
-	
-				if (isset($result)){
-					header("Content-Type: application/json; charset=utf-8");
-					$json = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Success SELECT', 'data' => $result), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
-				} else {
-					$detalle = array(
-						'tipo_codigo'           => '',
-						'tipo_estado_codigo'    => '',
-						'tipo_estado_nombre'    => '',
-						'tipo_orden'            => '',
-						'tipo_nombre'           => '',
-						'tipo_equivalente'      => '',
-						'tipo_dominio'          => '',
-						'tipo_observacion'      => '',
-						'auditoria_usuario'     => '',
-						'auditoria_fecha_hora'  => '',
-						'auditoria_ip'          => ''
-					);
-	
-					header("Content-Type: application/json; charset=utf-8");
-					$json = json_encode(array('code' => 204, 'status' => 'ok', 'message' => 'No hay registros', 'data' => $detalle), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
-				}
-	
-				$stmtPGSQL->closeCursor();
-				$stmtPGSQL = null;
-			} catch (PDOException $e) {
-				header("Content-Type: application/json; charset=utf-8");
-				$json = json_encode(array('code' => 204, 'status' => 'failure', 'message' => 'Error SELECT: '.$e), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
-			}
-	
-			$connPGSQL  = null;
-			
-			return $json;
+		$app->get('/v1/000/dominio', function($request) {	
+			header("Content-Type: application/json; charset=utf-8");
+        	$json = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Success SELECT', 'data' => 'result'), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
 		})->add(basicAuth());
 	
 	$app->run();
