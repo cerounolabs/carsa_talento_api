96c6d62fac254a82344104b7eb384a8eae459656
Christian Zelaya - PC-01 <christian@cerouno.com.py>
2020-03-25 21:44:56 -0400

    Feature Auth

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,5 @@
 <?php
-	require __DIR__.'/../appv1/get.php';			//SOLICITUD GET
+//	require __DIR__.'/../appv1/get.php';			//SOLICITUD GET
 	require __DIR__.'/../appv1/post.php';			//SOLICITUD POST
 	require __DIR__.'/../appv1/put.php';			//SOLICITUD PUT
 	require __DIR__.'/../appv1/delete.php';			//SOLICITUD DELETE
